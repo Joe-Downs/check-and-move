9e8700283bf87b22b0a45af97488bd75bd6b1203
Joe Downs <joedowns502@gmail.com>
2021-02-14 20:07:29 -0500

    check-and-move.py: add comment to clarify what the while loop does

--- check-and-move.py
+++ check-and-move.py
@@ -17,6 +17,10 @@ def splitFilesAndDir(combinedDir):
 fileList, dirList = list(), list()
 dirList = [input("What folder would you like to move? ")]
 
+# Goes through the array of directories (just one directory at
+# the beginning), adds each subdirectory to the array, and adds
+# the files to the file array. Once the files in a directory are
+# added, the directory is removed from the array.
 while dirList != []:
     print(dirList)
     nextDir = dirList[0]
